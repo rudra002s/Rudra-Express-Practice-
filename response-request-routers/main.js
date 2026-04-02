77d3f4e8541dc79953e42906265956f27a4600aa
rudra002s <rudras.1902@gmail.com>
2026-04-02 18:14:11 +0530

    feat: add initial package.json and HTML page for handling POST requests
    - Created package.json for the response-request-routers project with express as a dependency.
    - Added public/page.html containing a script to test POST requests to the server.

--- response-request-routers/main.js
+++ response-request-routers/main.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log("get request")
+  res.send('Hello World. Hows life?!')
+})
+
+app.post('/', (req, res) => {
+    console.log("post request")
+  res.send('Hello World post!')
 })
 
 app.listen(port, () => {
