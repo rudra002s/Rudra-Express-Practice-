fdeb54e66b871d4447439aa0f08274823fa9bd5c
rudra002s <rudras.1902@gmail.com>
2026-04-03 17:43:48 +0530

    fix: adjust indentation for console log statements in main.js

--- response-request-routers/main.js
+++ response-request-routers/main.js
@@ -26,16 +26,16 @@ app.use(express.static("public"))
 
 //this can also be clubbed as a single like given below
 app.get('/', (req, res) => {
-  console.log("get request")
+    console.log("get request")
   res.send('Hello World. Hows life?!')
 }).post('/', (req, res) => {
-  console.log("post request")
+    console.log("post request")
   res.send('Hello World post!')
 }).put('/', (req, res) => {
-  console.log("put request")
+    console.log("put request")
   res.send('Hello World put!')
 }).delete('/', (req, res) => {
-  console.log("delete request")
+    console.log("delete request")
   res.send('Hello World delete!')
 })
 
