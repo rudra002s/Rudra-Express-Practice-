192a59eb240c98582c9196ada6bbe2d58fbe864a
rudra002s <rudras.1902@gmail.com>
2026-04-03 12:00:24 +0530

    refactor: consolidate HTTP method routes in main.js and update fetch method in page.html

--- response-request-routers/main.js
+++ response-request-routers/main.js
@@ -4,14 +4,39 @@ const port = 3000
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-    console.log("get request")
-  res.send('Hello World. Hows life?!')
-})
+// app.get('/', (req, res) => {
+//     console.log("get request")
+//   res.send('Hello World. Hows life?!')
+// })
 
-app.post('/', (req, res) => {
-    console.log("post request")
+// app.post('/', (req, res) => {
+//     console.log("post request")
+//   res.send('Hello World post!')
+// })
+
+// app.put('/', (req, res) => {
+//     console.log("put request")
+//   res.send('Hello World put!')
+// })
+
+// app.delete('/', (req, res) => {
+//     console.log("delete request")
+//   res.send('Hello World delete!')
+// })
+
+//this can also be clubbed as a single like given below
+app.get('/', (req, res) => {
+  console.log("get request")
+  res.send('Hello World. Hows life?!')
+}).post('/', (req, res) => {
+  console.log("post request")
   res.send('Hello World post!')
+}).put('/', (req, res) => {
+  console.log("put request")
+  res.send('Hello World put!')
+}).delete('/', (req, res) => {
+  console.log("delete request")
+  res.send('Hello World delete!')
 })
 
 app.listen(port, () => {
