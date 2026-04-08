19f75f8de6115ccfc4de4782049936029f97e182
rudra002s <rudras.1902@gmail.com>
2026-04-08 13:56:04 +0530

    feat: add rudra.txt and improve middleware structure in main.js

--- middlewares/main.js
+++ middlewares/main.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
+//syntax for middleware
+app.use((req, res, next) => {
+    console.log('LOGGED')
+    next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello Ritu!')
+    res.send('Hello Ritu!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello about!')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
