4b5c60de0ebf70f22098cc4ce9acfb53538e61a6
rudra002s <rudras.1902@gmail.com>
2026-04-10 00:48:56 +0530

    feat: implement logging middleware and add blog routes

--- middlewares/main.js
+++ middlewares/main.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const birds = require('./routes/blog')
+const fs = require("fs")
 
 // app.use(express.static("public"))
+app.use('/birds', birds)
 
 //syntax for middleware in this both m1 and m2 will be shown
 
 //MIDDLEWARE 1 - logger for our app
 app.use((req, res, next) => {
-    console.log(`${Date.now()}`)
+    //wherever req.rudra will be added it will be displayed to that page
+    req.rudra=" I am smart"
+    // fs.writeFileSync("logs.txt",`${Date.now()} is a ${req.method}`) //this will send data to a txt file which is auto created after this command
+
+    //this will append
+    fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method}`)
+    console.log(`${Date.now()} is a ${req.method}`)
 
     //with res.send the response will be sent and it will stop here itself and next will not work
 
@@ -24,15 +33,15 @@ app.use((req, res, next) => {
     next()
 })
 app.get('/', (req, res) => {
-    res.send('Hello Ritu!')
+    res.send('Hello Ritu!' + req.rudra)
 })
 
 app.get('/about', (req, res) => {
-    res.send('Hello about!')
+    res.send('Hello about!' + req.rudra)
 })
 
 app.get('/contact', (req, res) => {
-    res.send('Hello contact!')
+    res.send('Hello contact!' + req.rudra)
 })
 
 app.listen(port, () => {
