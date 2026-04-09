d8bc1d69bcf8f285f65f7540ee2ba873d36f378b
rudra002s <rudras.1902@gmail.com>
2026-04-10 00:55:16 +0530

    feat: enhance logging middleware with additional request data

--- middlewares/main.js
+++ middlewares/main.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 //MIDDLEWARE 2
 app.use((req, res, next) => {
     console.log('m2')
+    req.rudra=" Ritu is smart too"
     next()
 })
 app.get('/', (req, res) => {
